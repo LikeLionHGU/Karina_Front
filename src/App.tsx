e1842e678b2612ef5107097f4c1361f4bdc8f7f8
lingardd09 <sherlockicker@gmail.com>
2025-08-16 22:02:13 +0900

    feat: 헤더, 푸터, 메인페이지 완료

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,37 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Landing from './pages/Landing'
-import Main from './pages/Home'
+import Landing from "./pages/Landing";
+import Home from "./pages/Home";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import styled from "styled-components";
 
-import './App.css'
+import "./App.css";
+
+const AppContainer = styled.div`
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+`;
+
+const MainContent = styled.main`
+  flex: 1;
+`;
 
 function App() {
-
   return (
-    <>
-      <BrowserRouter>
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route path="/main" element={<Main />} />
-        </Routes>
-      </div>
+    <BrowserRouter>
+      <AppContainer>
+        <Header />
+        <MainContent>
+          <Routes>
+            <Route path="/" element={<Landing />} />
+            <Route path="/home" element={<Home />} />
+          </Routes>
+        </MainContent>
+        <Footer />
+      </AppContainer>
     </BrowserRouter>
-    </>
-  )
+  );
 }
 
-export default App
+export default App;
